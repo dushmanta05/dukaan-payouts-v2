de27d70e4600c36c4cf2219d609936487872ff4f
Dushmanta <dushmanta0511@gmail.com>
2024-03-07 23:59:40 +0530

    chore: refactor dashboard with reusable component

--- components/dashboard/Dashboard.jsx
+++ components/dashboard/Dashboard.jsx
@@ -4,6 +4,36 @@ import Transaction from "./Transaction";
 import PaginationNumber from "./PaginationNumber";
 
 const Dashboard = () => {
+  const items = [
+    { title: "Order ID" },
+    {
+      title: "Order Date",
+      src: "/images/svg/small-down-triangle.svg",
+      dimension: 8,
+      alt: "small-down-triangle",
+    },
+    { title: "Order Amount", justifyEnd: true },
+    {
+      title: "Transaction Fees",
+      src: "/images/svg/small-info.svg",
+      dimension: 14,
+      alt: "small info",
+      justifyEnd: true,
+    },
+  ];
+
+  const paginationNumbers = [
+    { number: "1", className: "hidden md:flex" },
+    { number: "2", className: "hidden md:flex" },
+    { number: "...", className: "hidden md:flex" },
+    { number: "10", className: "flex bg-idColor text-white" },
+    { number: "11", className: "hidden md:flex" },
+    { number: "12", className: "hidden lg:flex" },
+    { number: "13", className: "hidden lg:flex" },
+    { number: "14", className: "hidden xl:flex" },
+    { number: "15", className: "hidden xl:flex" },
+  ];
+
   return (
     <div className="p-[32px] text-black">
       <div className="flex justify-between ">
@@ -19,8 +49,12 @@ const Dashboard = () => {
         </button>
       </div>
       <div className="pt-[24px] gap-[20px] flex flex-col md:flex-row justify-between">
-        <OrderSummaryCard title="Online orders" value="231" />
-        <OrderSummaryCard title="Amount received" value="₹23,92,312.19" />
+        {[
+          { title: "Online Orders", value: "231" },
+          { title: "Amount Received", value: "₹23,92,312.19" },
+        ].map((item, index) => (
+          <OrderSummaryCard key={index} title={item.title} value={item.value} />
+        ))}
       </div>
       <div className="pt-[32px]">
         <p className="text-customBlack text-xl font-medium pb-[20px]">
@@ -39,23 +73,36 @@ const Dashboard = () => {
                 <p className="text-searchId text-sm">Search by order ID...</p>
               </div>
               <div className="flex items-center gap-[12px]">
-                <div className="flex justify-center h-[40px] gap-[6px] px-[12px] py-[6px] items-center rounded border">
-                  <p className="text-subtitle text-base">Sort</p>
-                  <Image
-                    src="/images/svg/sort.svg"
-                    width={16}
-                    height={16}
-                    alt="sort"
-                  />
-                </div>
-                <div className="hidden md:flex h-[36px]  justify-center gap-[6px] px-[12px] py-[6px] items-center rounded border">
-                  <Image
-                    src="/images/svg/download.svg"
-                    width={20}
-                    height={20}
-                    alt="download"
-                  />
-                </div>
+                {[
+                  {
+                    src: "/images/svg/sort.svg",
+                    dimension: 16,
+                    alt: "sort",
+                    text: "Sort",
+                    class: "flex h-[40px]",
+                  },
+                  {
+                    src: "/images/svg/download.svg",
+                    dimension: 20,
+                    alt: "download",
+                    class: "hidden md:flex h-[36px]",
+                  },
+                ].map((item, index) => (
+                  <div
+                    key={index}
+                    className={`justify-center gap-[6px] px-[12px] py-[6px] items-center rounded border ${item.class}`}
+                  >
+                    {item.text && (
+                      <p className="text-subtitle text-base">{item.text}</p>
+                    )}
+                    <Image
+                      src={item.src}
+                      width={item.dimension}
+                      height={item.dimension}
+                      alt={item.alt}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -63,42 +110,31 @@ const Dashboard = () => {
           <div className="overflow-x-auto">
             <div className="min-w-[660px]">
               <div className="flex gap-[20px] md:gap-[40px] px-[12px] py-[10px] h-[48px] bg-secondary rounded ">
-                <div className="w-full flex">
-                  <p className="text-subtitle font-normal text-base">
-                    Order ID
-                  </p>
-                </div>
-                <div className="w-full flex items-center">
-                  <div className="flex justify-start items-center gap-[3px]">
-                    <p className="text-subtitle font-normal text-base">
-                      Order Date
-                    </p>
-                    <Image
-                      src="/images/svg/small-down-triangle.svg"
-                      width={8}
-                      height={8}
-                      alt="small-down-triangle"
-                    />
-                  </div>
-                </div>
-                <div className="w-full flex justify-end items-center">
-                  <p className="text-subtitle font-normal text-base">
-                    Order Amount
-                  </p>
-                </div>
-                <div className="w-full flex justify-end items-center">
-                  <div className="inline-flex items-center justify-start gap-[3px]">
-                    <p className="text-subtitle font-normal text-base">
-                      Transaction Fees
-                    </p>
-                    <Image
-                      src="/images/svg/small-info.svg"
-                      width={14}
-                      height={14}
-                      alt="small-info"
-                    />
-                  </div>
-                </div>
+                {items.map((item, index) => {
+                  const divClass = `w-full flex items-center ${
+                    item.justifyEnd ? "justify-end" : ""
+                  }`;
+                  const pClass = "text-subtitle font-normal text-base";
+                  const innerDivClass =
+                    "flex justify-start items-center gap-[3px]";
+                  return item.src ? (
+                    <div key={index} className={divClass}>
+                      <div className={innerDivClass}>
+                        <p className={pClass}>{item.title}</p>
+                        <Image
+                          src={item.src}
+                          width={item.dimension}
+                          height={item.dimension}
+                          alt={item.alt}
+                        />
+                      </div>
+                    </div>
+                  ) : (
+                    <div key={index} className={divClass}>
+                      <p className={pClass}>{item.title}</p>
+                    </div>
+                  );
+                })}
               </div>
               {new Array(11).fill(null).map((_, index) => (
                 <Transaction key={index} />
@@ -116,18 +152,13 @@ const Dashboard = () => {
               Previous
             </button>
             <div className="flex items-center gap-[8px] py-[2px]">
-              <PaginationNumber number="1" className="hidden md:flex" />
-              <PaginationNumber number="2" className="hidden md:flex" />
-              <PaginationNumber number="..." className="hidden md:flex" />
-              <PaginationNumber
-                number="10"
-                className="flex bg-idColor text-white"
-              />
-              <PaginationNumber number="11" className="hidden md:flex" />
-              <PaginationNumber number="12" className="hidden lg:flex" />
-              <PaginationNumber number="13" className="hidden lg:flex" />
-              <PaginationNumber number="14" className="hidden xl:flex" />
-              <PaginationNumber number="15" className="hidden xl:flex" />
+              {paginationNumbers.map(({ number, className }, index) => (
+                <PaginationNumber
+                  key={index}
+                  number={number}
+                  className={className}
+                />
+              ))}
             </div>
             <button className="flex gap-[6px] items-center rounded border pr-[12px] py-[6px] pl-[6px] ">
               Next
--- components/dashboard/Transaction.jsx
+++ components/dashboard/Transaction.jsx
@@ -1,18 +1,39 @@
 const Transaction = () => {
+  const transactionData = [
+    {
+      text: "#281209",
+      colorText: true,
+    },
+    {
+      text: "7 July, 2023",
+    },
+    {
+      text: "₹1,278.23",
+      justifyEnd: true,
+    },
+    {
+      text: "₹22",
+      justifyEnd: true,
+    },
+  ];
   return (
     <div className="flex gap-[40px] h-[48px] px-[12px] py-[14px] rounded border-b">
-      <div className="w-full flex">
-        <p className="text-idColor text-sm">#281209</p>
-      </div>
-      <div className="w-full flex items-center">
-        <p className="text-sm text-customBlack">7 July, 2023</p>
-      </div>
-      <div className="w-full flex justify-end items-center">
-        <p className="text-sm text-customBlack">₹1,278.23</p>
-      </div>
-      <div className="w-full flex justify-end items-center">
-        <p className="text-sm text-customBlack">₹22</p>
-      </div>
+      {transactionData.map((item, index) => (
+        <div
+          key={index}
+          className={`w-full flex items-center ${
+            item.justifyEnd && "justify-end"
+          }`}
+        >
+          <p
+            className={`text-sm ${
+              item.colorText ? "text-idColor" : "text-customBlack"
+            }`}
+          >
+            {item.text}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
